ca294d450cf17d9160439a3563a39efbe41f5e9c
Govind Kashyap <165226936+Kashyapji12@users.noreply.github.com>
2026-04-17 21:51:55 +0530

    Update effect.js

--- effect.js
+++ effect.js
@@ -7,13 +7,13 @@ $('document').ready(function(){
 		$(window).resize(function(){
 			 vw = $(window).width()/2;
 			$('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
-			$('#b11').animate({top:240, left: vw-350},500);
-			$('#b22').animate({top:240, left: vw-250},500);
-			$('#b33').animate({top:240, left: vw-150},500);
-			$('#b44').animate({top:240, left: vw-50},500);
-			$('#b55').animate({top:240, left: vw+50},500);
-			$('#b66').animate({top:240, left: vw+150},500);
-			$('#b77').animate({top:240, left: vw+250},500);
+			$('#b11').animate({top:240, left: vw-300},500);
+			$('#b22').animate({top:240, left: vw-200},500);
+			$('#b33').animate({top:240, left: vw-100},500);
+			$('#b44').animate({top:240, left: vw+0},500);
+			$('#b55').animate({top:240, left: vw+100},500);
+			$('#b66').animate({top:240, left: vw+200},500);
+			$('#b77').animate({top:240, left: vw+300},500);
 		});
 
 	$('#turn_on').click(function(){
@@ -86,7 +86,6 @@ $('document').ready(function(){
 			loopFive();
 		});
 	}
-
 	function loopSix() {
 		var randleft = 1000*Math.random();
 		var randtop = 500*Math.random();
@@ -106,11 +105,6 @@ $('document').ready(function(){
 		$('.balloon-border').animate({top:-500},8000);
 		$('#b1,#b4,#b5,#b7').addClass('balloons-rotate-behaviour-one');
 		$('#b2,#b3,#b6').addClass('balloons-rotate-behaviour-two');
-		// $('#b3').addClass('balloons-rotate-behaviour-two');
-		// $('#b4').addClass('balloons-rotate-behaviour-one');
-		// $('#b5').addClass('balloons-rotate-behaviour-one');
-		// $('#b6').addClass('balloons-rotate-behaviour-two');
-		// $('#b7').addClass('balloons-rotate-behaviour-one');
 		loopOne();
 		loopTwo();
 		loopThree();
@@ -126,6 +120,8 @@ $('document').ready(function(){
 
 	$('#cake_fadein').click(function(){
 		$('.cake').fadeIn('slow');
+		// Naya Logic: Cake ke saath Close-up photo aayegi
+		$('#photo1').fadeIn(3000); 
 		$(this).fadeOut('slow').delay(3000).promise().done(function(){
 			$('#light_candle').fadeIn('slow');
 		});
@@ -138,25 +134,27 @@ $('document').ready(function(){
 		});
 	});
 
-		
 	$('#wish_message').click(function(){
 		 vw = $(window).width()/2;
 
 		$('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
 		$('#b1').attr('id','b11');
-		$('#b2').attr('id','b22')
-		$('#b3').attr('id','b33')
-		$('#b4').attr('id','b44')
-		$('#b5').attr('id','b55')
-		$('#b6').attr('id','b66')
-		$('#b7').attr('id','b77')
-		$('#b11').animate({top:240, left: vw-350},500);
-		$('#b22').animate({top:240, left: vw-250},500);
-		$('#b33').animate({top:240, left: vw-150},500);
-		$('#b44').animate({top:240, left: vw-50},500);
-		$('#b55').animate({top:240, left: vw+50},500);
-		$('#b66').animate({top:240, left: vw+150},500);
-		$('#b77').animate({top:240, left: vw+250},500);
+		$('#b2').attr('id','b22');
+		$('#b3').attr('id','b33');
+		$('#b4').attr('id','b44');
+		$('#b5').attr('id','b55');
+		$('#b6').attr('id','b66');
+		$('#b7').attr('id','b77');
+		
+		// Balloons position for P-A-K-H-I
+		$('#b11').animate({top:240, left: vw-300},500);
+		$('#b22').animate({top:240, left: vw-200},500);
+		$('#b33').animate({top:240, left: vw-100},500);
+		$('#b44').animate({top:240, left: vw+0},500);
+		$('#b55').animate({top:240, left: vw+100},500);
+		$('#b66').animate({top:240, left: vw+200},500);
+		$('#b77').animate({top:240, left: vw+300},500);
+
 		$('.balloons').css('opacity','0.9');
 		$('.balloons h2').fadeIn(3000);
 		$(this).fadeOut('slow').delay(3000).promise().done(function(){
@@ -166,6 +164,9 @@ $('document').ready(function(){
 	
 	$('#story').click(function(){
 		$(this).fadeOut('slow');
+		// Naya Logic: Story ke saath Selfie photo aayegi
+		$('#photo2').fadeIn(2000); 
+		
 		$('.cake').fadeOut('fast').promise().done(function(){
 			$('.message').fadeIn('slow');
 		});
@@ -180,22 +181,12 @@ $('document').ready(function(){
 				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
 					$('.cake').fadeIn('fast');
 				});
-				
 			}
 			else{
 				msgLoop(i);
 			}			
-
 		});
-			// body...
 		}
-		
 		msgLoop(0);
-		
 	});
 });
-
-
-
-
-//alert('hello');
